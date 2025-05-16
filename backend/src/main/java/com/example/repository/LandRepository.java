e15f84dc3d7c65c64738b5e579e4bea1aef02329
Salma <salma3.azize3@gmail.com>
2025-05-17 00:15:45 +0100

    username field added to user

--- backend/src/main/java/com/example/repository/LandRepository.java
+++ backend/src/main/java/com/example/repository/LandRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 @Repository
-public interface LandRepository extends JpaRepository<Land, UUID> {
+public interface LandRepository extends JpaRepository<Land, Long> {
     List<Land> findByUser(User user);
 }
